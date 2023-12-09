5bd87262b24513847a78605193ae546d2f023ccc
Archit Sharma <sh02archit@gmail.com>
2023-12-09 17:24:24 +0530

    open ai integration done

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
-
+from openai import OpenAI
 
 def process_query(query: str) -> str:
-    # From GPT we will get the text for the query
-    # For demonstration purposes, simply returning the processed query
-    return f"Processed: {query}"
+    client = OpenAI()
+    
+    sys_prompt = 'You are a friendly chatbot. The user will either enquire or will have a natural conversation. Each query would be resolved by a different pipeline in the backend which will generate a sequence of API calls in json format. Your goal is to *just* generate brief text (upto 35 words) that precedes the actual answer. Do not make any assumptions but act as if you are looking into it.'
+    
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": sys_prompt},
+            {"role": "user", "content": query}
+        ]
+    )
+    return completion.choices[0].message.content
+    # return 'hi'
 
 
 def pipeline(query: str):
