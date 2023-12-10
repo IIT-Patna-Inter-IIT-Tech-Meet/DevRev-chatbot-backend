73e08577f10330ea6cc0d2f0d61782e72ae22052
Archit Sharma <sh02archit@gmail.com>
2023-12-10 20:14:52 +0530

    Added retriever and assistant to code.

--- main.py
+++ main.py
@@ -2,34 +2,97 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 from openai import OpenAI
+import time
+
+ASSISTANT_ID = 'asst_VSDOllgBf0GkI50zVnBJdJ5N'
+RETRIEVER_ID = 'asst_xMGkLqbVNc1EYAg14AFxpEdr'
+
+client = OpenAI()
+thread = client.beta.threads.create()
+ret_thread = client.beta.threads.create()
+
+def wait_on_run(run, thread):
+    while run.status == "queued" or run.status == "in_progress":
+        run = client.beta.threads.runs.retrieve(
+            thread_id=thread.id,
+            run_id=run.id,
+        )
+        time.sleep(0.5)
+    return run
+
 
 def process_query(query: str) -> str:
-    client = OpenAI()
+    # client = OpenAI()
+    # sys_prompt = 'You are a friendly chatbot. The user will either enquire or will have a natural conversation. Each query would be resolved by a different pipeline in the backend which will generate a sequence of API calls in json format. Your goal is to *just* generate brief text (upto 35 words) that precedes the actual answer. Do not make any assumptions but act as if you are looking into it.'
     
-    sys_prompt = 'You are a friendly chatbot. The user will either enquire or will have a natural conversation. Each query would be resolved by a different pipeline in the backend which will generate a sequence of API calls in json format. Your goal is to *just* generate brief text (upto 35 words) that precedes the actual answer. Do not make any assumptions but act as if you are looking into it.'
-    
-    completion = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": sys_prompt},
-            {"role": "user", "content": query}
-        ]
+    # completion = client.chat.completions.create(
+    #     model="gpt-3.5-turbo",
+    #     messages=[
+    #         {"role": "system", "content": sys_prompt},
+    #         {"role": "user", "content": query}
+    #     ]
+    # )
+    # return completion.choices[0].message.content
+
+    message = client.beta.threads.messages.create(
+        thread_id=thread.id,
+        role="user",
+        content=query
     )
-    return completion.choices[0].message.content
-    # return 'hi'
+    run = client.beta.threads.runs.create(
+        thread_id=thread.id,
+        assistant_id=ASSISTANT_ID,
+    )
+    
+    wait_on_run(run, thread)
+    
+    messages = client.beta.threads.messages.list(
+        thread_id=thread.id,
+        order='desc',
+    )
+    
+    print(messages)
+    idx, ite = -1, 0
+    for i, msg in enumerate(messages):
+        if msg.content[0].text.value == query:
+            idx += i
+    
+    for msg in messages:
+        if ite == idx:
+            return msg.content[0].text.value
+        ite += 1
 
 
+def retriever(query: str):
+    message = client.beta.threads.messages.create(
+        thread_id=ret_thread.id,
+        role="user",
+        content=query
+    )
+    run = client.beta.threads.runs.create(
+        thread_id=ret_thread.id,
+        assistant_id=RETRIEVER_ID,
+    )
+    wait_on_run(run, ret_thread)
+    messages = client.beta.threads.messages.list(
+        thread_id=ret_thread.id,
+        order='desc',
+    )
+    
+    print(messages)
+    idx, ite = -1, 0
+    for i, msg in enumerate(messages):
+        if msg.content[0].text.value == query:
+            idx += i
+    
+    for msg in messages:
+        if ite == idx:
+            return {'tools': msg.content[0].text.value}
+        ite += 1
+    
+    
 def pipeline(query: str):
-    # Your ML pipeline logic goes here
-    # For demonstration purposes, simply returning the a sample json
-    return {
-        "intent": {
-            "name": 'account',
-            "confidence": 0.9999999999999999,
-        },
-        "entities": {},
-    }
-
+    return retriever(query)
 
 app = FastAPI()
 
